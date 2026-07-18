c58db8a4a1e90c3f7f3e73cfe19646cea7a9b4a8
cacortes <cacortes@student.42malaga.com>
2026-07-18 20:36:55 +0200

    map validation in progress

--- src/Persona_A/main.c
+++ src/Persona_A/main.c
@@ -6,7 +6,7 @@
 /*   By: cacortes <cacortes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/12 09:31:07 by cacortes          #+#    #+#             */
-/*   Updated: 2026/07/17 18:06:32 by cacortes         ###   ########.fr       */
+/*   Updated: 2026/07/18 20:36:01 by cacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,10 +48,11 @@ int file_walls(char *file, char *compass)
     int     i;
 
     found = 0;
+	//printf("LA VARIABLE ES %d\n", found);
     fd_file = open(file, O_RDONLY);
     if (fd_file == -1)
         return (1);
-
+	
     line = get_next_line(fd_file);
     while (line)
     {
@@ -190,6 +191,166 @@ int file_color(char *file, char *compass)
     return (!found);
 }
 
+/*Map things*/
+int	is_map_line(char *line)
+{
+	while (*line == ' ')
+		line++;
+
+	while (*line)
+	{
+		if (!ft_strchr(" 01NSEW\n", *line))
+			return (0);
+		line++;
+	}
+	return (1);
+}
+int	count_map_lines(char *file)
+{
+	int		fd;
+	int		count;
+	char	*line;
+
+	fd = open(file, O_RDONLY);
+	count = 0;
+
+	line = get_next_line(fd);
+	while (line)
+	{
+		if (is_map_line(line))
+			count++;
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	return (count);
+}
+
+char	**create_map(char *file)
+{
+	int	num_lines;
+	int	fd_file;
+	char *line;
+	char **map;
+	int	i;
+
+	num_lines = count_map_lines(file);
+	i = 0;
+	map = malloc(sizeof(char *) * (num_lines + 1));
+	if (!map)
+		return (NULL);
+	fd_file = open(file, O_RDONLY);
+	line = get_next_line(fd_file);
+	while (line)
+	{
+		if (is_map_line(line))
+			map[i++] = line;
+		else
+			free(line);
+		line = get_next_line(fd_file);
+	}
+	map[i] = NULL;
+	return (map);
+}
+
+int	map_width(char **map)
+{
+	int	i;
+	int	max;
+	int	len;
+
+	i = 0;
+	max = 0;
+	while (map[i])
+	{
+		len = ft_strlen(map[i]);
+		if (len > max)
+			max = len;
+		i++;
+	}
+	return (max);
+}
+
+char	**expand_map(char **map)
+{
+	int	w;
+	int h;
+	char	**new_map;
+	int 	i;
+	int		x
+	int		y;
+
+	i = 0;
+	x = 0;
+	y = 0;
+	h = count_words(map);
+	w = map_width(map);
+	new_map = malloc(sizeof(char *) * (h + 3));
+	while (i < h + 2)
+	{
+		new_map[i] = malloc(sizeof(char) * (old_width + 3));
+		i++;
+	}
+	while(y < h + 2)
+	{
+		while(x < w + 2)
+		{
+			new_map[y][x] = ' ';
+			x++;
+		}
+		new_map[y][w + 2] = '\0';
+		y++;
+	}
+	for (int y = 0; y < old_height; y++)
+	{
+    	for (int x = 0; x < old_width; x++)
+    	{
+    	    new_map[y + 1][x + 1] = map[y][x];
+	    }
+	}
+	return (new_map);
+}
+
+static void	flood_fill(char **ctx, int x, int y)
+{
+	char	c;
+
+	if (x < 0 || x >= ctx->width || y < 0 || y >= ctx->height)
+		return ;
+	c = ctx->map[y][x];
+	if (c == '1' || c == 'V')
+		return ;
+	if (c == ' ')
+		return ;
+	if (c == 'C')
+		ctx->found->collectible++;
+	else if (c == 'E')
+		ctx->found->exit++;
+	ctx->map[y][x] = 'V';
+	flood_fill(ctx, x + 1, y);
+	flood_fill(ctx, x - 1, y);
+	flood_fill(ctx, x, y + 1);
+	flood_fill(ctx, x, y - 1);
+}
+
+int	file_map(char *file)
+{
+	char **map;
+	char **map_exp;
+	int	i;
+
+	map = create_map(file);
+	i = 0;
+	while (map[i])
+	{
+		printf("%s\n", map[i]);
+		i++;
+	}
+	map_exp = expand_map(map);
+	flood_fill(new_map, 0, 0);
+	return (0);
+}
+
 int	parser_file_content(char *file)
 {
 	if (file_walls(file, "NO") != 0)
@@ -210,15 +371,18 @@ int	parser_file_content(char *file)
 	else if (file_color(file, "C") != 0)
 		printf("Error\nThere is an error in your .cub file"
 			" regarding ceiling color.\n");
+	else if (file_map(file) != 0)
+		printf("Error\nThere is an error in your .cub file"
+			" regarding the map.\n");
 	else
 		printf("GOOD\n");
-
 	return (0);
 }
 
 int	parser_file_extension(char *file)
 {
 	char	*start;
+	int		fd;
 
 	start = file + ft_strlen(file) - 4;
 	if (!ft_strnstr(start, ".cub", 4))
@@ -226,6 +390,13 @@ int	parser_file_extension(char *file)
 		printf("Error\n"
 			"The file you are trying to enter"
 			" is not valid or doesn't exists.\n");
+		return (1);
+	}
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+	{
+		printf("Error\n"
+			"The file can not be opened\n");
 		return (1);
 	}
 	return (0);
@@ -240,7 +411,8 @@ int	main(int argc, char **argv)
 			" is not compatible with the program.\n");
 		return (1);
 	}
-	parser_file_extension(argv[1]);
+	if (parser_file_extension(argv[1]) != 0)
+		return (1);
 	parser_file_content(argv[1]);
 	return (0);
 }*/
